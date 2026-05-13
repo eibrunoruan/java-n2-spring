ff67fbeeed25c8e5e1d4ae4328f38b5cc9544aa3
Bruno Ruan dos Santos - MAC <eibrunoruan@gmail.com>
2026-05-13 19:35:19 -0300

    feat: adicionando a entidade movie e seus dados

---
+++ src/main/java/br/org/catolicasc/movie_catalog/model/Movie.java
@@ -0,0 +1,78 @@
+package br.org.catolicasc.movie_catalog.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    private String director;
+
+    private Integer releaseYear;
+
+    private Double rating;
+
+    @ManyToOne
+    @JoinColumn(name = "genre_id")
+    private Genre genre;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public Integer getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Integer releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,9 @@ INSERT INTO genre (name) VALUES ('Comedy');
 INSERT INTO genre (name) VALUES ('Drama');
 INSERT INTO genre (name) VALUES ('Science Fiction');
 INSERT INTO genre (name) VALUES ('Horror');
+
+INSERT INTO movie (title, director, release_year, rating, genre_id) VALUES ('Inception', 'Christopher Nolan', 2010, 9.3, 4);
+INSERT INTO movie (title, director, release_year, rating, genre_id) VALUES ('Interstellar', 'Christopher Nolan', 2014, 9.5, 4);
+INSERT INTO movie (title, director, release_year, rating, genre_id) VALUES ('Gladiator', 'Ridley Scott', 2000, 8.5, 1);
+INSERT INTO movie (title, director, release_year, rating, genre_id) VALUES ('Parasite', 'Bong Joon-ho', 2019, 9.0, 3);
+INSERT INTO movie (title, director, release_year, rating, genre_id) VALUES ('Fright Night', 'Tom Holland', 1985, 7.2, 5);
